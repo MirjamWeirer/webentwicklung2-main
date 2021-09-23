3c27c053731cbd9399a977490449822704f9acfe
Weirer Mirjam <s44673@edu.campus02.at>
2021-09-23 11:13:29 +0200

    übung2 player

--- webentwicklung2-main/HtmlJsTemplate/script/main.js
+++ webentwicklung2-main/HtmlJsTemplate/script/main.js
@@ -1,33 +1,48 @@
 "use strict";
 
 // hier kommen ihre Skripte hin
-const a = Math.random(), b = Math.random();
-console.log(a);
-console.log(b);
-let abfrage = prompt("Welche Rechnungsoperation möchten sie verwenden");
-console.log(abfrage);
-if (abfrage == "+"){
-    console.log(add(a,b));
-}else if(abfrage == "-"){
-    console.log(sub(a,b));
-}else if (abfrage == "/"){
-    console.log(diq(a,b));
-}else if(abfrage == "*"){
-    console.log(mul(a,b));
-}
-function add(a,b){
-    return a+b;
-}
-
-function sub (a,b){
-    return (a-b);
-}
-
-function div (a,b){
-    return (a/b);
-}
-
-function mul(a,b){
-    return (a*b);
+//const a = Math.random(), b = Math.random();
+//console.log(a);
+//console.log(b);
+//let abfrage = prompt("Welche Rechnungsoperation möchten sie verwenden");
+//console.log(abfrage);
+//if (abfrage == "+"){
+//    console.log(add(a,b));
+//}else if(abfrage == "-"){
+//    console.log(sub(a,b));
+//}else if (abfrage == "/"){
+//    console.log(diq(a,b));
+//}else if(abfrage == "*"){
+//    console.log(mul(a,b));
+//}
+//function add(a,b){
+//    return a+b;
+//}
+//
+//function sub (a,b){
+//    return (a-b);
+//}
+//
+//function div (a,b){
+//    return (a/b);
+//}
+//
+//function mul(a,b){
+//    return (a*b);
+//}
+let numberOfPlayer = prompt("Anzahl?");
+class Player{
+    constructor(name, handcard, points){
+        this.name = name;
+        this.handcard = handcard;
+        this.points = points;
+    }
+    numberPlayer(numberOfPlayer){
+        for(var number = 0; number < numberOfPlayer; number ++){
+            name = prompt("Namen eingeben:");
+            this.name = name;
+            this.handcard = [3,4,5];
+        }
+    }
 }
 
