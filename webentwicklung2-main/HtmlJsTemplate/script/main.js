b07aed29e7b02fb78d1f8749a91d0a28606c11cf
Weirer Mirjam <s44673@edu.campus02.at>
2021-09-23 08:44:48 +0200

    rechenoperationen

--- webentwicklung2-main/HtmlJsTemplate/script/main.js
+++ webentwicklung2-main/HtmlJsTemplate/script/main.js
@@ -1,3 +1,32 @@
 "use strict";
 
 // hier kommen ihre Skripte hin
+const a = Math.random(), b = Math.random();
+
+let abfrage = prompt("Welche Rechnungsoperation möchten sie verwenden");
+console.log(abfrage);
+function add(a,b){
+    return a+b;
+}
+
+function sub (a,b){
+    return a-b;
+}
+
+function div (a,b){
+    return a/b;
+}
+
+function mul(a,b){
+    return a*b;
+}
+
+if (abfrage == "+"){
+    console.log(add());
+}else if(abfrage == "-"){
+    console.log(sub());
+}else if (abfrage == "/"){
+    console.log(diq());
+}else if(abfrage == "*"){
+    console.log(mul());
+}
