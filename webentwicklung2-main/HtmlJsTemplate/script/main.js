4b35036244bebceede56dc980a3e098e47a301bc
Weirer Mirjam <s44673@edu.campus02.at>
2021-09-23 10:13:10 +0200

    operationen

--- webentwicklung2-main/HtmlJsTemplate/script/main.js
+++ webentwicklung2-main/HtmlJsTemplate/script/main.js
@@ -1,3 +1,33 @@
 "use strict";
 
 // hier kommen ihre Skripte hin
+const a = Math.random(), b = Math.random();
+console.log(a);
+console.log(b);
+let abfrage = prompt("Welche Rechnungsoperation möchten sie verwenden");
+console.log(abfrage);
+if (abfrage == "+"){
+    console.log(add(a,b));
+}else if(abfrage == "-"){
+    console.log(sub());
+}else if (abfrage == "/"){
+    console.log(diq());
+}else if(abfrage == "*"){
+    console.log(mul());
+}
+function add(a,b){
+    return a+b;
+}
+
+function sub (a,b){
+    return (a-b);
+}
+
+function div (a,b){
+    return (a/b);
+}
+
+function mul(a,b){
+    return (a*b);
+}
+
